c7d4f37a4e113ad9c927bc2431843b1839562643
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-07-19 22:47:51 +0200

    Created IFundaParser interface

---
+++ FundaFifo.TTFunction/Common/Interfaces/IFundaParser.cs
@@ -0,0 +1,11 @@
+﻿using FundaFifo.TTFunction.Common.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace FundaFifo.TTFunction.Common.Interfaces
+{
+	public interface IFundaParser
+    {
+        Task<IEnumerable<PropertyDto>> GetPropertiesAsync();
+    }
+}
