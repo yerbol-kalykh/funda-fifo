d3dd71b2b86c57f9bfb66900639151b534621a29
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-07-19 22:47:38 +0200

    Created PropertyDto class

---
+++ FundaFifo.TTFunction/Common/Dtos/PropertyDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FundaFifo.TTFunction.Common.Dtos
+{
+	public class PropertyDto
+	{
+		public string Title { get; set; }
+
+		public string Postcode { get; set; }
+
+		public Uri Url { get; set; }
+
+		public string Price { get; set; }
+
+		public static PropertyDto Create(string title, string postcode, string uri, string price)
+		{
+			return new PropertyDto
+			{
+				Title = title,
+				Postcode = postcode,
+				Url = new Uri(uri),
+				Price = price
+			};
+		}
+	}
+}
