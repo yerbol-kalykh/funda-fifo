ae5e6ffe752adb3180aac4cef75de9e7d96c84f2
Yerbol Kalykhbergenov <yerbol.kalykh@gmail.com>
2021-07-19 23:10:36 +0200

    Added DI for FundaParser

---
+++ FundaFifo.TTFunction/Startup.cs
@@ -0,0 +1,17 @@
+﻿using FundaFifo.TTFunction.Common.Helpers;
+using FundaFifo.TTFunction.Common.Interfaces;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(FundaFifo.TTFunction.Startup))]
+
+namespace FundaFifo.TTFunction
+{
+	public class Startup : FunctionsStartup
+	{
+		public override void Configure(IFunctionsHostBuilder builder)
+		{
+			builder.Services.AddSingleton<IFundaParser, FundaParser>();
+		}
+	}
+}
